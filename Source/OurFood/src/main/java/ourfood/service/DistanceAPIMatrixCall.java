a7e934870042cf9e0661ffd796958f8f2020fb95
k.rachna27@gmail.com <k.rachna27@gmail.com>
2016-10-03 02:09:40 +0530

    Created a class to call Google Distance API.

--- Source/OurFood/src/main/java/ourfood/service/DistanceAPIMatrixCall.java
+++ Source/OurFood/src/main/java/ourfood/service/DistanceAPIMatrixCall.java
@@ -11,21 +11,39 @@ import javax.net.ssl.HttpsURLConnection;
 public class DistanceAPIMatrixCall {
 
 	private final String USER_AGENT = "Mozilla/5.0";
+	private final String API_KEY = "AIzaSyCpmt9JgfPw_MJJo6iSB0pB0uVPS89hZ0E";
 
 	//public DistanceAPIMatrixCall() {
 	//	System.setProperty("http.proxyHost", "proxy.iiit.ac.in");
 	//	System.setProperty("http.proxyPort", "8080"); 
 	//}
 
-	// HTTP GET request
-	//private void sendGet(double Olat,double Olong,double Dlat,double Dlong ) throws Exception {
-	private void sendGet() throws Exception {
-		//Olat = 17.444792;
-		//Olong =	78.348310;
-		//Dlat = 17.596589;
-		//Dlong = 78.124105;
+	public static void main(String[] args) {
+		System.out.println("Qqwwere");
+	}
 
-	 	String url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=Vancouver+BC|Seattle&destinations=San+Francisco|Victoria+BC&mode=bicycling&language=fr-FR&key=AIzaSyCpmt9JgfPw_MJJo6iSB0pB0uVPS89hZ0E" ;
+	// HTTP GET request
+	private String sendGet(LocationHolder[] origins, LocationHolder[] destinations) throws Exception {
+		String[] urlTokens = new String[4];
+		String url = "";
+		int i;
+		
+		urlTokens[0] = "https://maps.googleapis.com/maps/api/distancematrix/json?";
+		urlTokens[1] = "origins=";
+		urlTokens[2] = "destinations=";
+		urlTokens[3] = "key=" + API_KEY;
+
+		for(i = 0; i < origins.length - 1; i++)
+			urlTokens[1] += origins[i].latitude + "," + origins[i].longitude + "|";
+		urlTokens[i] = origins[i].latitude + "," + origins[i].longitude + "&";
+
+		for(i = 0; i < destinations.length - 1; i++)
+			urlTokens[1] += destinations[i].latitude + "," + destinations[i].longitude + "|";
+		urlTokens[i] = origins[i].latitude + "," + origins[i].longitude + "&";
+		
+		for(i = 0; i < 4; i++)
+			url += urlTokens[i];
+
 		//String url = "https://maps.googleapis.com/maps/api/directions/json?origin=" + 17.444792 + "," + 78.348310 + "&destination=" + 17.596589 + "," + 78.124105 + "&key=AIzaSyCxfMjWup0K4Dg_T7qkA4mLZ1FW-lOBdVQ";
 
 		URL obj = new URL(url);
@@ -49,8 +67,6 @@ public class DistanceAPIMatrixCall {
 		}
 		in.close();
 
-		//print result
-		System.out.println(response.toString());
-
+		return response.toString();
 	}
 }
